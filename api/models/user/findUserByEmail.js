26e1a5d28d49d5c7e608f74c9c80ce1f460730e3
cristianocsl <cslcristiano@gmail.com>
2022-04-22 14:51:57 -0300

    refactor: retira linhas em branco

--- api/models/user/findUserByEmail.js
+++ api/models/user/findUserByEmail.js
@@ -3,9 +3,7 @@ const connection = require('../connection');
 const findUserByEmail = async (email) => {
   const db = await connection();
   const repetedEmail = await db.collection('users').findOne({ email });
-
   if (!repetedEmail) return null;
-
   return repetedEmail;
 };
 
