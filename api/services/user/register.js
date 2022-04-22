aa7e209ed880980b43870dd408104abb697469f0
cristianocsl <cslcristiano@gmail.com>
2022-04-22 16:51:45 -0300

    feat: adiciona classe de lançamento de erros no método register

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -1,7 +1,10 @@
 const { register: create, findUserByEmail } = require('../../models/user');
+const { ApiError: { SendToErrorMiddleware } } = require('../../error/apiError');
+const { EMAIL_EXISTING } = require('../../error/msgCodeError');
 
 const register = async ({ name, email, password }) => {
-  await findUserByEmail(email);
+  const repetedEmail = await findUserByEmail(email);
+  if (repetedEmail) return SendToErrorMiddleware(EMAIL_EXISTING);
   await create({ name, email, password });
   return { name: `${name.split(' ')[0]}`, token: 'abc' };
 };
