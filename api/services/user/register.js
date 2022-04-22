92ca66842435c88a1a2c7d42c0e281b88328fb54
cristianocsl <cslcristiano@gmail.com>
2022-04-22 16:56:28 -0300

    feat: adiciona criptografia à senha do usuário

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -1,3 +1,4 @@
+const { encryptor } = require('../utilities/bcryptFunction');
 const { register: create, findUserByEmail } = require('../../models/user');
 const { ApiError: { SendToErrorMiddleware } } = require('../../error/apiError');
 const { EMAIL_EXISTING } = require('../../error/msgCodeError');
@@ -5,7 +6,7 @@ const { EMAIL_EXISTING } = require('../../error/msgCodeError');
 const register = async ({ name, email, password }) => {
   const repetedEmail = await findUserByEmail(email);
   if (repetedEmail) return SendToErrorMiddleware(EMAIL_EXISTING);
-  await create({ name, email, password });
+  await create({ name, email, password: await encryptor(password) });
   return { name: `${name.split(' ')[0]}`, token: 'abc' };
 };
 
