c7353eb7a01d3451577be3bc2bce2e2f64ef0c06
cristianocsl <cslcristiano@gmail.com>
2022-04-22 02:18:55 -0300

    feat: implementa método findUserByEmail

---
+++ api/models/user/findUserByEmail.js
@@ -0,0 +1,12 @@
+const connection = require('../connection');
+
+const findUserByEmail = async (email) => {
+  const db = await connection();
+  const repetedEmail = await db.collection('users').findOne({ email });
+
+  if (!repetedEmail) return null;
+
+  return repetedEmail;
+};
+
+module.exports = findUserByEmail;
