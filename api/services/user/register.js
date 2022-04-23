5ed43ae82f8d276d5d72daa0729b50c58bb84c3c
cristianocsl <cslcristiano@gmail.com>
2022-04-23 18:02:23 -0300

    feat: adiciona model register na service de registro de usuário

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -1,12 +1,7 @@
-const { encryptor } = require('../utilities/bcryptFunction');
-const { register: create, findUserByEmail } = require('../../models/user');
-const { ApiError: { SendToErrorMiddleware } } = require('../../error/apiError');
-const { EMAIL_EXISTING } = require('../../error/msgCodeError');
+const { register: create } = require('../../models/user');
 
-const register = async ({ name, email, password }) => {
-  const repetedEmail = await findUserByEmail(email);
-  if (repetedEmail) return SendToErrorMiddleware(EMAIL_EXISTING);
-  await create({ name, email, password: await encryptor(password) });
+const register = async ({ name, email, password, securityPhrase }) => {
+  await create({ name, email, password, securityPhrase });
   return { name: `${name.split(' ')[0]}`, token: 'abc' };
 };
 
