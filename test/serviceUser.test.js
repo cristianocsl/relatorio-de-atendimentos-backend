771b3b53ac5301822c21e433f708939d2b25fbc9
cristianocsl <cslcristiano@gmail.com>
2022-04-23 18:03:05 -0300

    test: adiciona teste de funcionamento do método register de service

--- test/serviceUser.test.js
+++ test/serviceUser.test.js
@@ -1,25 +1,31 @@
 const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
 const RegisterModel = require('../api/models/user');
 const RegisterService = require('../api/services/user');
 
-jest.useRealTimers();
-
 describe('Testes de verificação da camada service para registro e login:', function () {
+  let connectionMock;
   const payload = {
-    _id: '604cb554311d68f491ba5781',
     name: 'Cristiano',
     email: 'cslcristiano@gmail.com',
     password: '123456',
-    confirmPassword: '123456',
     securityPhrase: 'meu-segredo',
   };
 
   beforeAll(async function () {
-    sinon.stub(RegisterModel, 'register').resolves({ ...payload });
+    const DBSERVER = await MongoMemoryServer.create();
+    const URLMock = DBSERVER.getUri();
+    connectionMock = MongoClient
+    .connect(URLMock, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
 
-    sinon.stub(RegisterModel, 'findUserByEmail').resolves({ ...payload });
-    console.log(RegisterService.register(payload));
+    sinon.stub(RegisterModel, 'register').resolves({ ...payload });
   });
 
   afterAll(function () {
@@ -29,8 +35,9 @@ describe('Testes de verificação da camada service para registro e login:', fun
   describe('- ao realizar o cadastro', function () {
     test('verifica o funcionamento do método register', async function () {
       const response = await RegisterService.register({ ...payload });
+
+      expect(response).toHaveProperty('name');
       expect(response.name).toEqual('Cristiano');
-      console.log(response);
       expect(response.token).toEqual('abc');
     });
   });
