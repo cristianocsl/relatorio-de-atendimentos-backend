25686d05524eadc7430d26fc261c1080c168bfb6
cristianocsl <cslcristiano@gmail.com>
2022-04-24 14:48:54 -0300

    feat: adiciona gerador de token

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -1,14 +1,17 @@
 const { register: create, findUserByEmail } = require('../../models/user');
 const { encryptor } = require('../utilities/bcryptFunction');
-
+const { tokenGenerator } = require('../utilities/tokenGenerator');
 const { ApiError } = require('../../error/apiError');
 const { EMAIL_EXISTING } = require('../../error/msgCodeError');
 
 const register = async ({ name, email, password, securityPhrase }) => {
   const user = await findUserByEmail(email);
   if (user) return ApiError.SendToErrorMiddleware(EMAIL_EXISTING);
-  await create({ name, email, password: await encryptor(password), securityPhrase });
-  return { name: `${name.split(' ')[0]}`, token: 'abc' };
+  const { _id } = await create(
+    { name, email, password: await encryptor(password), securityPhrase },
+  );
+  const token = tokenGenerator({ _id, name, email });
+  return { name: `${name.split(' ')[0]}`, token };
 };
 
 module.exports = register;
