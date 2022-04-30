59a47fbecbd88d7c7878816fd9c3e6a0d6840f30
cristianocsl <cslcristiano@gmail.com>
2022-04-29 22:44:12 -0300

    refactor: remove validação com joi

--- api/controllers/user/register.js
+++ api/controllers/user/register.js
@@ -3,13 +3,12 @@ const { register: create } = require('../../services/user');
 const writeSuccessMsg = require('../../services/utilities/successMsg');
 
 const register = async (req, res) => {
-  const reqBody = req.body;
-
+  const reqBody = req.body; 
   try {
     const user = await create(reqBody);
     return res.status(CREATED).json(writeSuccessMsg(user.name));
-  } catch (err) {
-    return res.status(BAD_REQUEST).json({});
+  } catch (error) {
+    return res.status(BAD_REQUEST).json({ message: error.message });
   }
 };
 
