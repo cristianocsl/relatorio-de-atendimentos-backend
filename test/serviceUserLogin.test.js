4306a076b319d6d2257e3db9f4ba524df34d0fff
cristianocsl <cslcristiano@gmail.com>
2022-05-02 19:09:31 -0300

    test: adiciona testes em caso de falha no login

--- test/serviceUserLogin.test.js
+++ test/serviceUserLogin.test.js
@@ -5,15 +5,10 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const RegisterModel = require('../api/models/user');
 const RegisterService = require('../api/services/user');
 const ApiError = require('../api/error/apiError');
-const { USER_DOES_NOT_EXIST } = require('../api/error/msgCodeError');
+const { USER_DOES_NOT_EXIST, INCORRECT_LOGIN } = require('../api/error/msgCodeError');
 
 describe('Testes de verificação da camada service para registro e login:', function () {
   let connectionMock;
-
-  const payload = {
-    email: 'cslcristiano@gmail.com',
-    password: '123456',
-  };
 
   beforeAll(async function () {
     const DBSERVER = await MongoMemoryServer.create();
@@ -24,9 +19,13 @@ describe('Testes de verificação da camada service para registro e login:', fun
       useUnifiedTopology: true,
     });
     
-    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
-    sinon.stub(ApiError, 'SendToErrorMiddleware').resolves(USER_DOES_NOT_EXIST);
-
+    sinon.stub(MongoClient, 'connect').returns(connectionMock);
+    sinon.stub(ApiError, 'SendToErrorMiddleware')
+      .onCall(0)
+        .resolves(USER_DOES_NOT_EXIST)
+      .onCall(1)
+        .resolves(INCORRECT_LOGIN);
+    
     await RegisterModel.register({
       name: 'Cristiano',
       email: 'cslcristiano@gmail.com',
@@ -35,30 +34,36 @@ describe('Testes de verificação da camada service para registro e login:', fun
     });
   });
 
-  afterAll(function () {
+  afterAll(async function () {
+    MongoClient.connect.restore();
     ApiError.SendToErrorMiddleware.restore();
-  });
-
-  describe('- ao realizar o login com sucesso, verifica que o método login', function () {
-    test('retorna as chaves "name" e "token"', async function () {
-      const response = await RegisterService.login({ ...payload }); // nesse caso, o uso de cópia (spread) não faz diferença.
-
-      expect(response).toHaveProperty('name');
-      expect(response).toHaveProperty('token');
-    });
   });
 
   describe('- ao realizar o login com um email que não existe no banco de dados', function () {
     test('retorna um código e uma mensagem de erro', async function () {
       const response = await RegisterService.login({
         email: 'email_diferente@gmail.com',
-        senha: '123456',
+        password: '123456',
       });
-
+      
       expect(response).toHaveProperty('code');
       expect(response).toHaveProperty('message');
       expect(response.code).toEqual(USER_DOES_NOT_EXIST.code);
       expect(response.message).toEqual(USER_DOES_NOT_EXIST.message);
+    });
+  });
+  
+  describe('- ao realizar o login com uma senha errada', function () {
+    test('retorna um código e uma mensagem de erro', async function () {
+      const response = await RegisterService.login({
+        email: 'cslcristiano@gmail.com',
+        password: 'xxxxxx',
+      });
+      
+      expect(response).toHaveProperty('code');
+      expect(response).toHaveProperty('message');
+      expect(response.code).toEqual(INCORRECT_LOGIN.code);
+      expect(response.message).toEqual(INCORRECT_LOGIN.message);
     });
   });
 });
