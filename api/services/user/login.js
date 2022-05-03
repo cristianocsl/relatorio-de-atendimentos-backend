90691797f7db455932c54c5bc878000fa62a571f
cristianocsl <cslcristiano@gmail.com>
2022-05-02 22:51:04 -0300

    feat: adiciona função de comparação de senhas e método de lançamento de erro

--- api/services/user/login.js
+++ api/services/user/login.js
@@ -1,12 +1,18 @@
+const { comparator } = require('../utilities/bcryptFunction');
+const { tokenGenerator } = require('../utilities/tokenGenerator');
 const { findUserByEmail } = require('../../models/user');
 const ApiError = require('../../error/apiError');
-const { USER_DOES_NOT_EXIST } = require('../../error/msgCodeError');
+const { USER_DOES_NOT_EXIST, INCORRECT_LOGIN } = require('../../error/msgCodeError');
 
-const login = async ({ email/* , password: inputPassword */ }) => {
+const login = async ({ email, password: inputPassword }) => {
   const user = await findUserByEmail(email);
   if (!user) return ApiError.SendToErrorMiddleware(USER_DOES_NOT_EXIST);
+  const matchPassword = await comparator(inputPassword, user.password);
+  if (!matchPassword) return ApiError.SendToErrorMiddleware(INCORRECT_LOGIN);
+  const { _id, name } = user;
+  const token = tokenGenerator({ _id, name, email });
 
-  return { name: '', token: '' };
+  return { name: `${name.split(' ')[0]}`, token };
 };
 
 module.exports = login;
