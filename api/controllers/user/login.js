10358e6592cefd30c5ca665fe4e1bc034affcc82
cristianocsl <cslcristiano@gmail.com>
2022-05-17 19:58:18 -0300

    feat: adiciona controller de usuário

---
+++ api/controllers/user/login.js
@@ -0,0 +1,15 @@
+const { OK } = require('http-status-codes').StatusCodes;
+const { login: loginWith } = require('../../services/user');
+
+const login = async (req, res) => {
+  const reqBody = req.body;
+  try {
+    const result = await loginWith(reqBody);
+    return res.status(OK).json(result);
+  } catch (err) {
+    console.log(err);
+    return res.status(err.code).json({ message: err.message });
+  }
+};
+
+module.exports = login;
