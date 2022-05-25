3c9c7072358ed8070ca3714b24baa7ed90c0af20
cristianocsl <cslcristiano@gmail.com>
2022-05-25 15:59:24 -0300

    refactor: renomeia variável de rota de usuário

--- api/controllers/user/router.js
+++ api/controllers/user/router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const register = require('./register');
 const login = require('./login');
 
-const routerUser = express.Router({ mergeParams: true });
+const userRouter = express.Router({ mergeParams: true });
 
-routerUser.post('/register', register); 
-routerUser.post('/login', login);
+userRouter.post('/register', register); 
+userRouter.post('/login', login);
 
-module.exports = routerUser;
+module.exports = userRouter;
