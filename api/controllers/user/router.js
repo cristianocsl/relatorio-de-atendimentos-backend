dc90e4cb3a98688becc78e5b6c2f19f46d390af7
cristianocsl <cslcristiano@gmail.com>
2022-05-25 15:56:14 -0300

    feat: adiciona rota de login

--- api/controllers/user/router.js
+++ api/controllers/user/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const register = require('./register');
+const login = require('./login');
 
 const routerRegister = express.Router({ mergeParams: true });
 
 routerRegister.post('/register', register); 
+routerRegister.post('/login', login);
 
 module.exports = routerRegister;
