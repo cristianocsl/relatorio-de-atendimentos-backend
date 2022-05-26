fd08662ae21f76661075a28eeef239917ea76c96
cristianocsl <cslcristiano@gmail.com>
2022-05-26 00:41:00 -0300

    feat: adiciona model de registro de paciente

--- api/models/patient/index.js
+++ api/models/patient/index.js
@@ -1,6 +1,6 @@
 const connection = require('../connection');
 
-module.exports.registerUser = async (patientInfos) => {
+module.exports.registerPacient = async (patientInfos) => {
   const db = await connection();
   const { insertedId } = await db.collection('patients').insertOne(patientInfos);
   return { _id: insertedId };
