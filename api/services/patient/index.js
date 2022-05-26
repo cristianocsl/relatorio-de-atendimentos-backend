47e18c8a3672bd7ee857995fbeb3c955f2b5a877
cristianocsl <cslcristiano@gmail.com>
2022-05-26 00:46:00 -0300

    feat: adiciona service para cadastro de pacientes

---
+++ api/services/patient/index.js
@@ -0,0 +1,23 @@
+const { registerPacient: register } = require('../../models/patient');
+
+module.exports.registerPatient = async (payload) => {
+  const {
+    unitPrice,
+    serviceGoal: { monthly, weekly },
+    servicePerformed: { monthly: monthlyDone, weekly: weeklyDone },
+   } = payload;
+  const prevTotalPrice = unitPrice * monthly;
+  const doneTotalPrice = unitPrice * monthlyDone;
+  const newPayload = {
+    ...payload,
+    prevTotalPrice,
+    doneTotalPrice,
+    servicePending: {
+      weekly: weekly - weeklyDone,
+      monthly: monthly - monthlyDone,
+    },
+  };
+  await register(newPayload);
+
+  return newPayload;
+};
