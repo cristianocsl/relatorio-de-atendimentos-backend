e57252a7aa09c6ecb84aad94d1247f1c3ef5a69c
cristianocsl <cslcristiano@gmail.com>
2022-05-25 23:48:59 -0300

    refactor: modifica nome da função e modo de exportar

--- api/models/patient/index.js
+++ api/models/patient/index.js
@@ -1,5 +1,7 @@
-const register = require('./register');
+const connection = require('../connection');
 
-module.exports = {
-  register,
+module.exports.registerUser = async (patientInfos) => {
+  const db = await connection();
+  const { insertedId } = await db.collection('patients').insertOne(patientInfos);
+  return { _id: insertedId };
 };
