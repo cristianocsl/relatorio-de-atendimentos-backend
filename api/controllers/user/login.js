9724686ad3da6d7a5df05cc2c155dc23e107a110
cristianocsl <cslcristiano@gmail.com>
2022-05-25 22:41:11 -0300

    refactor: remove console.log

--- api/controllers/user/login.js
+++ api/controllers/user/login.js
@@ -7,7 +7,6 @@ const login = async (req, res) => {
     const result = await loginWith(reqBody);
     return res.status(OK).json(result);
   } catch (err) {
-    console.log(err);
     return res.status(err.code).json({ message: err.message });
   }
 };
