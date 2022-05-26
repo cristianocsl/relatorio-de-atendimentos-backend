c2dda0debebcafea661dcfedde72b97fb0b01f19
cristianocsl <cslcristiano@gmail.com>
2022-05-25 23:47:48 -0300

    refactor: exclui arquivos para refatoramento

--- api/models/patient/register.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../connection');
-
-const register = async (patientInfos) => {
-  const db = await connection();
-  const { insertedId } = await db.collection('patients').insertOne(patientInfos);
-  return { _id: insertedId };
-};
-
-module.exports = register;
