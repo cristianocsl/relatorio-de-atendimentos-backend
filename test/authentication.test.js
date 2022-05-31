c85e02a5c31d31614b89346195b3575d34dbe3f6
cristianocsl <cslcristiano@gmail.com>
2022-05-31 18:43:51 -0300

    test: adiciona teste de middleware de autenticação

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,17 +1,18 @@
 const sinon = require('sinon');
+const jwt = require('jsonwebtoken');
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 require('dotenv').config();
 
-const { TOKEN_NOT_FOUND } = require('../api/error/msgCodeError');
+const { TOKEN_NOT_FOUND, INVALID_TOKEN } = require('../api/error/msgCodeError');
 const ApiError = require('../api/error/apiError');
 const controller = require('../api/controllers/user');
+const { authentication } = require('../api/middlewares/auth');
 
 describe('Teste da função authentication', function () {
   let connectionMock;
   let request = {};
-
-  const response = {};
+  let response = {};
 
   const authPayload = {
     _id: '6282d5893854824c30bfe84f',
@@ -43,6 +44,29 @@ describe('Teste da função authentication', function () {
     request.user = { ...authPayload };
   });
 
+  test('- retorna status 401 e uma mensagem de erro, se o token não for encontrado',
+  async function () {
+    request = { headers: {} };
+    response = {};
+    const next = () => {};
+    const result = await authentication(request, response, next);
+    console.log('result 1 ', result);
+    expect(result.code).toEqual(401);
+    expect(result.message).toEqual('Token não encontrado!');
+  });
+
+  test('- verifica que o token é validado e retorna chave user com dados do usuário',
+  async function () {
+    sinon.stub(jwt, 'sign').resolves('um token qualquer gerado com sucesso');
+    sinon.stub(jwt, 'verify').resolves({ ...authPayload });
+    request = { headers: { authorization: 'token inválido' } };
+    response = {};
+    const next = () => {};
+    await authentication(request, response, next);
+
+    expect(request).toHaveProperty('user');
+  });
+
   test('- retorna uma mensagem de erro, se o token não for encontrado', async function () {
     request = { headers: { authorization: '' } };
     const spy = sinon.spy();
@@ -52,6 +76,18 @@ describe('Teste da função authentication', function () {
         sinon.match.has(
           'message',
           'Token não encontrado!',
+          ));
+  });
+
+  test('- retorna uma mensagem de erro, se o token for inválido', async function () {
+    request = { headers: { authorization: 'inválido' } };
+    const spy = sinon.spy();
+      spy(INVALID_TOKEN);
+
+      sinon.assert.calledWith(spy,
+        sinon.match.has(
+          'message',
+          'Token inválido ou expirado!',
           ));
   });
 });
