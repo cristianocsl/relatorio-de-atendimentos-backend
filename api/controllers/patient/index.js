8e2ec91203005dcc9e00f9283478113c2c945c63
cristianocsl <cslcristiano@gmail.com>
2022-05-31 18:54:34 -0300

    feat: adiciona dados do usuário no para relacionar com o seu paciente cadastrado

--- api/controllers/patient/index.js
+++ api/controllers/patient/index.js
@@ -3,9 +3,8 @@ const { registerPatient: register } = require('../../services/patient');
 
 module.exports.registerPatient = async (req, res) => {
   const { body, user } = req;
-  console.log('req >>', user);
   try {
-    const patient = await register({ ...body });
+    const patient = await register({ ...body, user });
     return res.status(CREATED).json(patient);
   } catch (err) {
     return res.status(BAD_REQUEST).json({ message: err.message });
