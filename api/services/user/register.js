f79a82e841dcb6849a6c99e7e63c10f364043c07
cristianocsl <cslcristiano@gmail.com>
2022-06-01 17:08:10 -0300

    feat: adiciona propriedade userId e message e remove propriedade token de  função de registro

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -1,17 +1,20 @@
 const { register: create, findUserByEmail } = require('../../models/user');
 const { encryptor } = require('../utilities/bcryptFunction');
-const { tokenGenerator } = require('../utilities/tokenGenerator');
 const ApiError = require('../../error/apiError');
 const { EMAIL_EXISTING } = require('../../error/msgCodeError');
 
 const register = async ({ name, email, password, securityPhrase }) => {
   const user = await findUserByEmail(email);
   if (user) return ApiError.SendToErrorMiddleware(EMAIL_EXISTING);
-  const { _id } = await create(
+  const { _id: userId } = await create(
     { name, email, password: await encryptor(password), securityPhrase },
   );
-  const token = tokenGenerator({ _id, name, email });
-  return { name: `${name.split(' ')[0]}`, token };
+  return {
+    name: `${name.split(' ')[0]}`,
+    email,
+    userId,
+    message: 'Usuário cadastrado com sucesso!',
+  };
 };
 
 module.exports = register;
