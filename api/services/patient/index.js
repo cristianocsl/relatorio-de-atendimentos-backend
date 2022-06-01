783a4a10e8ea7809d1455916665f870902335e47
cristianocsl <cslcristiano@gmail.com>
2022-06-01 12:30:37 -0300

    feat: refatora registerPatient reduzindo linhas da função

--- api/services/patient/index.js
+++ api/services/patient/index.js
@@ -1,6 +1,21 @@
 const { registerPatient: register } = require('../../models/patient');
+const { findPatient } = require('../../models/patient/findPatient');
+
+const newPayload = ({ 
+    payload, weekly, weeklyDone, monthly, monthlyDone, prevTotalPrice, doneTotalPrice,
+  }) => ({
+  ...payload,
+    prevTotalPrice,
+    doneTotalPrice,
+    servicePending: {
+      weekly: weekly - weeklyDone,
+      monthly: monthly - monthlyDone,
+    },
+  });
 
 module.exports.registerPatient = async (payload) => {
+  const patient = await findPatient({ patient: payload.patient });
+  console.log('patient', patient);
   const {
     unitPrice,
     serviceGoal: { monthly, weekly },
@@ -8,16 +23,11 @@ module.exports.registerPatient = async (payload) => {
    } = payload;
   const prevTotalPrice = unitPrice * monthly;
   const doneTotalPrice = unitPrice * monthlyDone;
-  const newPayload = {
-    ...payload,
-    prevTotalPrice,
-    doneTotalPrice,
-    servicePending: {
-      weekly: weekly - weeklyDone,
-      monthly: monthly - monthlyDone,
-    },
-  };
-  await register(newPayload);
+  const myNewPayload = newPayload({
+    payload, weekly, weeklyDone, monthly, monthlyDone, prevTotalPrice, doneTotalPrice,
+  });
 
-  return { ...newPayload, message: 'Paciente cadastrado com sucesso!' };
+  await register(myNewPayload);
+
+  return { ...myNewPayload, message: 'Paciente cadastrado com sucesso!' };
 };
