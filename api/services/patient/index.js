607f0816e8b0ac95db1c3e32c75b4014b8be5752
cristianocsl <cslcristiano@gmail.com>
2022-06-01 13:25:52 -0300

    feat: adiciona condicional para verificar se o paciente já está cadastrado

--- api/services/patient/index.js
+++ api/services/patient/index.js
@@ -1,3 +1,6 @@
+const ApiError = require('../../error/apiError');
+const { EXISTING_PATIENT } = require('../../error/msgCodeError');
+
 const { registerPatient: register } = require('../../models/patient');
 const { findPatient } = require('../../models/patient/findPatient');
 
@@ -14,15 +17,19 @@ const newPayload = ({
   });
 
 module.exports.registerPatient = async (payload) => {
-  const patient = await findPatient({ patient: payload.patient });
-  console.log('patient', patient);
+  const patient = await findPatient({ ...payload });
+
+  if (patient) return ApiError.SendToErrorMiddleware(EXISTING_PATIENT);
+
   const {
     unitPrice,
     serviceGoal: { monthly, weekly },
     servicePerformed: { monthly: monthlyDone, weekly: weeklyDone },
    } = payload;
+
   const prevTotalPrice = unitPrice * monthly;
   const doneTotalPrice = unitPrice * monthlyDone;
+
   const myNewPayload = newPayload({
     payload, weekly, weeklyDone, monthly, monthlyDone, prevTotalPrice, doneTotalPrice,
   });
