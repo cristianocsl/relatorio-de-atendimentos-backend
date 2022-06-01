316a40f3c9281d72b1f8ed4d26fbbd99149e5b3c
cristianocsl <cslcristiano@gmail.com>
2022-06-01 00:15:20 -0300

    refactor: apenas a chave userId é passada para o body de registro de paciente

--- api/controllers/patient/index.js
+++ api/controllers/patient/index.js
@@ -2,9 +2,9 @@ const { CREATED, BAD_REQUEST } = require('http-status-codes').StatusCodes;
 const { registerPatient: register } = require('../../services/patient');
 
 module.exports.registerPatient = async (req, res) => {
-  const { body, user } = req;
+  const { body, user: { _id: userId } } = req;
   try {
-    const patient = await register({ ...body, user });
+    const patient = await register({ ...body, userId });
     return res.status(CREATED).json(patient);
   } catch (err) {
     return res.status(BAD_REQUEST).json({ message: err.message });
