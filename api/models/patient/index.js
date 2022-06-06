6a6340659188e57d17f3668178b5eb752bb54fab
cristianocsl <cslcristiano@gmail.com>
2022-06-06 02:37:44 -0300

    feat: adiciona função que encontra paciente pelo id

--- api/models/patient/index.js
+++ api/models/patient/index.js
@@ -1,9 +1,11 @@
 const { findPatient } = require('./findPatient');
 const { registerPatient } = require('./registerPatient');
 const { updatePatient } = require('./updatePatient');
+const { findPatientById } = require('./findPatientById');
 
 module.exports = {
   registerPatient,
   findPatient,
   updatePatient,
+  findPatientById,
 };
