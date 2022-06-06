77894aa89093807e5685c018c597f561d2bd0079
cristianocsl <cslcristiano@gmail.com>
2022-06-06 00:53:06 -0300

    refactor: adiciona arquivo index.js para importação/exportação

--- api/controllers/patient/index.js
+++ api/controllers/patient/index.js
@@ -1,12 +1,5 @@
-const { CREATED, BAD_REQUEST } = require('http-status-codes').StatusCodes;
-const { registerPatient: register } = require('../../services/patient');
+const { registerPatient } = require('./registerPatient');
 
-module.exports.registerPatient = async (req, res) => {
-  const { body, user: { _id: userId } } = req;
-  try {
-    const patient = await register({ ...body, userId });
-    return res.status(CREATED).json(patient);
-  } catch (err) {
-    return res.status(BAD_REQUEST).json({ message: err.message });
-  }
+module.exports = {
+  registerPatient,
 };
