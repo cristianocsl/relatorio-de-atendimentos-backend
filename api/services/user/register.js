540c1657c87b80fbfe413d8edcf426bb48621909
cristianocsl <cslcristiano@gmail.com>
2022-06-10 10:49:16 -0300

    refactor: munda nome da função findUserById por findUser

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -1,10 +1,10 @@
-const { register: create, findUserByEmail } = require('../../models/user');
+const { register: create, findUser } = require('../../models/user');
 const { encryptor } = require('../utilities/bcryptFunction');
 const ApiError = require('../../error/apiError');
 const { EMAIL_EXISTING } = require('../../error/msgCodeError');
 
 const register = async ({ name, email, password, securityPhrase }) => {
-  const user = await findUserByEmail(email);
+  const user = await findUser(email);
   if (user) return ApiError.SendToErrorMiddleware(EMAIL_EXISTING);
   const { _id: userId } = await create(
     { name, email, password: await encryptor(password), securityPhrase },
