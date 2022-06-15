a215ae0e19bc30022fd1cbedfd9cea1dae275716
cristianocsl <cslcristiano@gmail.com>
2022-06-15 20:19:42 -0300

    fix: remove lançamento de erro em caso de array vazio

--- api/services/patient/getAllPatientsFromUserId.js
+++ api/services/patient/getAllPatientsFromUserId.js
@@ -1,10 +1,3 @@
 const { getAllPatientsFromUserId: getAll } = require('../../models/patient');
-const ApiError = require('../../error/apiError');
-const { EMPTY_PATIENT_LIST } = require('../../error/msgCodeError');
 
-module.exports.getAllPatientsFromUserId = async (userId) => {
-  const patients = await getAll(userId);
-  if (!patients.length) return new ApiError(EMPTY_PATIENT_LIST);
-
-  return patients;
-};
+module.exports.getAllPatientsFromUserId = async (userId) => getAll(userId);
