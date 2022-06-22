3b77f744ced9b1350a46c8fbb4fc227d614d34ae
cristianocsl <cslcristiano@gmail.com>
2022-06-21 23:27:41 -0300

    feat: adiciona função de validação dos campos de login

--- api/services/user/login.js
+++ api/services/user/login.js
@@ -2,10 +2,13 @@ const bcrypt = require('bcrypt');
 
 const { tokenGenerator } = require('../utilities/tokenGenerator');
 const { findUser } = require('../../models/user');
+
 const ApiError = require('../../error/apiError');
 const { USER_DOES_NOT_EXIST, INCORRECT_PASSWORD } = require('../../error/msgCodeError');
+const { validateLogin } = require('../validations/validateLogin');
 
 const login = async ({ email, password: inputPassword }) => {
+  await validateLogin({ email, inputPassword });
   const user = await findUser(email);
   
   if (!user) throw new ApiError(USER_DOES_NOT_EXIST);
