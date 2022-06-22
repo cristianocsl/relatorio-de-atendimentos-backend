0db5ca1fcaf9e62c743433c0c8f397762b07a3d9
cristianocsl <cslcristiano@gmail.com>
2022-06-22 00:00:22 -0300

    feat: adiciona validação de email com regex

--- api/services/validations/validateLogin.js
+++ api/services/validations/validateLogin.js
@@ -1,9 +1,13 @@
 const ApiError = require('../../error/apiError');
-const { EMPTY_FIELD, INVALID_PASSWORD } = require('../../error/msgCodeError');
+const { EMPTY_FIELD, INVALID_PASSWORD, INVALID_EMAIL } = require('../../error/msgCodeError');
+
+// eslint-disable-next-line max-len
+const REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 module.exports.validateLogin = async (body) => {
   const { email, inputPassword } = body;
   if (!email.length) throw new ApiError(EMPTY_FIELD);
+  if (!REGEX.test(email)) throw new ApiError(INVALID_EMAIL);
   if (!inputPassword.length) throw new ApiError(EMPTY_FIELD);
   if (inputPassword.length < 6) throw new ApiError(INVALID_PASSWORD);
 };
