14244af7d2bc22f9f877285062f6e893cf5d89fb
cristianocsl <cslcristiano@gmail.com>
2022-06-25 20:58:44 -0300

    feat: adiciona a chave updatedAt ao atualizar algum dados do pacientes

--- api/services/patient/updatePatient.js
+++ api/services/patient/updatePatient.js
@@ -1,6 +1,7 @@
 const ApiError = require('../../error/apiError');
 const { INEXISTING_PATIENT, USERID_DOES_NOT_MATCH } = require('../../error/msgCodeError');
 const { updatePatient: update, findPatientById } = require('../../models/patient');
+const { convertOneDate } = require('../utilities/workingWithDates');
 
 module.exports.updatePatient = async (patientId, payload) => {
   const patient = await findPatientById(patientId);
@@ -9,7 +10,9 @@ module.exports.updatePatient = async (patientId, payload) => {
   const { userId, _id } = patient;
 
   if (userId !== payload.userId) throw new ApiError(USERID_DOES_NOT_MATCH);
-  await update(patientId, payload);
-  
-  return { ...payload, userId, _id };
+  const updatedAt = new Date().toISOString();
+
+  await update(patientId, { ...payload, updatedAt });
+
+  return { ...payload, userId, _id, updatedAt: convertOneDate(updatedAt) };
 };
