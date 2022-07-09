4131b36885bed07ff7fe7addac2f6ca6aa9cd207
cristianocsl <cslcristiano@gmail.com>
2022-07-09 00:50:56 -0300

    feat: adiciona um array com dados de finanças a função getAllPatientsFromUserId

--- api/services/patient/getAllPatientsFromUserId.js
+++ api/services/patient/getAllPatientsFromUserId.js
@@ -1,3 +1,9 @@
 const { getAllPatientsFromUserId: getAll } = require('../../models/patient');
+const { getFinances } = require('../../models/finances');
 
-module.exports.getAllPatientsFromUserId = async (userId) => getAll(userId);
+module.exports.getAllPatientsFromUserId = async (userId) => {
+  const patients = await getAll(userId);
+  const finances = await getFinances(userId);
+
+  return { patients, finances };
+};
