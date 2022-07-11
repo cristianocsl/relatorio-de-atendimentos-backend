453cdd2d6bac06775bdb89fbbe1a00bbbbfa6fa8
cristianocsl <cslcristiano@gmail.com>
2022-07-11 16:41:54 -0300

    feat: adiciona chave com token ao retorno da função

--- api/services/user/register.js
+++ api/services/user/register.js
@@ -2,6 +2,7 @@ const { register: create, findUser } = require('../../models/user');
 const { encryptor } = require('../utilities/bcryptFunction');
 const ApiError = require('../../error/apiError');
 const { EMAIL_EXISTING } = require('../../error/msgCodeError');
+const { tokenGenerator } = require('../utilities/tokenGenerator');
 
 const writeSuccessMsg = require('../utilities/successMsg');
 const { validateRegister } = require('../validations/validateRegister');
@@ -10,14 +11,18 @@ const register = async ({ name, email, password }) => {
   await validateRegister({ name, email, password });
   const user = await findUser(email);
   if (user) throw new ApiError(EMAIL_EXISTING);
-  const { _id: userId } = await create(
+  const { _id } = await create(
     { name, email, password: await encryptor(password) },
   );
+
+  const token = tokenGenerator({ _id, name, email });
+
   return {
     name: `${name.split(' ')[0]}`,
     email,
-    userId,
+    userId: _id,
     message: writeSuccessMsg(name),
+    token,
   };
 };
 
