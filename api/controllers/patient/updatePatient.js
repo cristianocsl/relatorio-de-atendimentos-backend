2592de46d55c4413b13298165e325a9e3c73b0eb
cristianocsl <cslcristiano@gmail.com>
2022-08-16 14:41:16 -0300

    refactor: muda mensagem de atualização de dados de paciente

--- api/controllers/patient/updatePatient.js
+++ api/controllers/patient/updatePatient.js
@@ -5,7 +5,7 @@ module.exports.updatePatient = async (req, res) => {
   try {
     const { body, params: { patientId }, user: { _id: userId } } = req;
     const patient = await update(patientId, { ...body, userId });
-    const successMsg = { message: 'Paciente atualizado com sucesso!' };
+    const successMsg = { message: 'Dados atualizados com sucesso!' };
     return res.status(OK).json({ ...patient, ...successMsg });
   } catch (err) {
     return res.status(err.code).json({ message: err.message });
